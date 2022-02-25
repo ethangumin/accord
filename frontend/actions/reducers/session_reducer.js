ff0c7f34c8f86129143eee578a6929c74cc2736f
egumin <ethangumin@gmail.com>
2022-02-25 17:56:34 -0500

    create usersReducer and sessionReducer

---
+++ frontend/actions/reducers/session_reducer.js
@@ -0,0 +1,19 @@
+import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from "../session_actions";
+
+const SessionReducer = (oldState = { id: null }) => {
+  Object.freeze(oldState);
+  let newState = Object.assign({}, oldState);
+
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      newState[id] = action.user.id;
+      return newState;
+    case LOGOUT_CURRENT_USER:
+      newState[id] = null;
+      return newState;
+    default:
+      return oldState;
+  }
+};
+
+export default SessionReducer;
---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,37 @@
+import * as SessionApiUtil from "../util/session_api_util";
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "REMOVE_CURRENT_USER";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
+
+const receiveCurrentUser = (user) => ({
+  type: RECEIVE_CURRENT_USER,
+  user: user,
+});
+
+const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER,
+});
+
+const receiveErrors = (errors) => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors: errors,
+});
+
+export const login = (user) => (dispatch) =>
+  receiveCurrentUser(user).then(
+    (user) => dispatch(SessionApiUtil.login(user)),
+    (err) => dispatch(receiveErrors(err.responseJSON))
+  );
+
+export const logout = () => (dispatch) =>
+  logoutCurrentUser().then(
+    (user) => dispatch(SessionApiUtil.login()),
+    (err) => dispatch(receiveErrors(err.responseJSON))
+  );
+
+export const signup = (user) => (dispatch) =>
+  receiveCurrentUser(user).then(
+    (user) => dispatch(SessionApiUtil.signup(user)),
+    (err) => dispatch(receiveErrors(err.responseJSON))
+  );
