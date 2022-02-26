2e205e3a5a81361fc9aab2e9f91889ef7c203447
egumin <ethangumin@gmail.com>
2022-02-26 18:03:05 -0500

    protect /login and /signup

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import SplashContainer from "./splash/splash_container"
-import SignUpFormContainer from './login_signup/signup_form_container'
-import LoginFormContainer from './login_signup/login_form_container'
+import { AuthRoute } from "../util/route_util";
+import SplashContainer from "./splash/splash_container";
+import SignUpFormContainer from "./login_signup/signup_form_container";
+import LoginFormContainer from "./login_signup/login_form_container";
 import ServerIndexContainer from "./servers/server_index_container";
 
 const App = () => {
@@ -11,8 +12,8 @@ const App = () => {
       {/* <SplashContainer /> */}
       <Switch>
         <Route exact path="/" component={SplashContainer} />
-        <Route path="/signup" component={SignUpFormContainer} />
-        <Route path="/login" component={LoginFormContainer} />
+        <AuthRoute path="/signup" component={SignUpFormContainer} />
+        <AuthRoute path="/login" component={LoginFormContainer} />
         <Route path="/servers" component={ServerIndexContainer} />
       </Switch>
     </div>
