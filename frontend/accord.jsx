07071e49cb80238cd513d59a81bc8c678a2780c0
egumin <ethangumin@gmail.com>
2022-02-25 20:06:55 -0500

    backend auth

--- frontend/accord.jsx
+++ frontend/accord.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as SessionApiUtil from "../frontend/util/session_api_util"
+import configureStore from "./store/store";
+// import * as SessionApiUtil from "../frontend/util/session_api_util"
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
 
-  window.signup = SessionApiUtil.signup
-  window.login = SessionApiUtil.login;
-  window.logout = SessionApiUtil.logout;
+  // window.signup = SessionApiUtil.signup
+  // window.login = SessionApiUtil.login;
+  // window.logout = SessionApiUtil.logout;
+
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 
   ReactDOM.render(<h1>Welcome to Accord</h1>, root);
 });
