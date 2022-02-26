b136567b0f8170a01461f22e143e4986d3d28bbe
egumin <ethangumin@gmail.com>
2022-02-25 22:08:38 -0500

    splash base styling

--- frontend/accord.jsx
+++ frontend/accord.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from "./store/store";
-// import * as SessionApiUtil from "../frontend/util/session_api_util"
+import Root from "./components/root";
+// import * as SessionApiUtil from "../frontend/util/session_api_util";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
+  const store = configureStore();
 
   // window.signup = SessionApiUtil.signup
   // window.login = SessionApiUtil.login;
   // window.logout = SessionApiUtil.logout;
 
-  const store = configureStore();
   window.getState = store.getState;
   window.dispatch = store.dispatch;
 
-  ReactDOM.render(<h1>Welcome to Accord</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
---
+++ frontend/components/splash/splash.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Splash = ({ currentUser, logout }) => {
+  const enterButton = currentUser ? "Open Discord" : "Login";
+
+  return (
+    <div className="splash__bg">
+      <div className="splash container">
+        <div className="splash__header">
+          <h1>Accord</h1>
+          <ul className="splash__nav">
+            <li>Download</li>
+            <li>Nitro</li>
+            <li>Safety</li>
+            <li>Support</li>
+            <li>Blog</li>
+            <li>Careers</li>
+          </ul>
+          <Link
+            className="splash__enter-btn"
+            to={currentUser ? "/servers" : "/login"}
+          >
+            {enterButton}
+          </Link>
+        </div>
+        <div className="splash__content">
+          <h1>Imagine A Place...</h1>
+          <p>
+            ipsum Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut,
+            magnam eum numquam exercitationem neque nemo inventore quisquam
+            optio tempore placeat nesciunt dolor earum quibusdam consectetur
+            sequi fugiat tenetur quasi quis?
+          </p>
+          <div className="splash__btns">
+            <input
+              type="button"
+              value="Download"
+              className="splash__download-btn"
+            />
+            <input
+              type="button"
+              value="Open Accord in your browser"
+              className="splash__open-browser-btn"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Splash;
