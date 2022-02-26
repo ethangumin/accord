7fa2eb52dbc9b00ee126a016d4678f6d07d0a616
egumin <ethangumin@gmail.com>
2022-02-26 11:23:32 -0500

    test branch push

---
+++ frontend/components/login_signup/login_form_container.js
@@ -0,0 +1,14 @@
+import { connect } from "react-redux";
+import LoginForm from "./login_form";
+
+const mSTP = (state, ownProps) => {
+  return {
+    errors: state.errors.session,
+  };
+};
+
+const mDTP = (dispatch, ownProps) => {
+    return {
+        login: 
+    }
+}
