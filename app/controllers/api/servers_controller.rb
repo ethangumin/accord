43e844bc27460ec9fec4e10e6da09bb8e5aab82f
egumin <ethangumin@gmail.com>
2022-02-28 12:17:33 -0500

    can create a server on backend

---
+++ app/controllers/api/servers_controller.rb
@@ -0,0 +1,21 @@
+class Api::ServersController < ApplicationController
+    def create
+        @server = Server.new(server_params)
+        @server.creator_id = current_user.id
+        if @server.save
+            render :show
+            # render json: ["Create Server Success"]
+        else
+            render json: ["Invalid Server Params"], status: 404
+        end
+    end
+
+    # def show
+    #     @server = Server.find_by(id: params[:id]);
+    # end
+
+    private
+    def server_params
+        params.require(:server).permit(:server_name)
+    end
+end
