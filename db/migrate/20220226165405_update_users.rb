93d72e9f259d7afbc1e798fe7780c15ed7f5531d
egumin <ethangumin@gmail.com>
2022-02-28 17:01:19 -0500

    update migrations

--- db/migrate/20220226165405_update_users.rb
+++
@@ -1,6 +0,0 @@
-class UpdateUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :email, :string, null: false, unique: true
-    add_index :users, :email, unique: true
-  end
-end
