a2f08faa39f84718117fa8ebee83cae793e8164e
egumin <ethangumin@gmail.com>
2022-03-06 23:04:15 -0500

    new server added to application state

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -14,27 +14,45 @@ const usersReducer = (oldState = {}, action) => {
     case RECEIVE_USER:
       newState[action.user.id] = action.user;
       return newState;
-    // case RECEIVE_SERVER:
-    //   const newServer = {
-    //     id: action.server.id,
-    //     serverName: action.server.serverName,
-    //     channels: action.server.channels,
-    //   };
-    //   Object.values(newState)[0].servers.push(newServer);
-    //   return newState;
-    // case RECEIVE_CHANNEL:
-    //   // debugger;
-    //   const newChannel = {
-    //     id: action.channel.id,
-    //     channelName: action.channel.channelName,
-    //     serverId: action.channel.serverId,
-    //   };
-    //   const currentServer = Object.values(newState)[0].servers.find(
-    //     (server) => server.id === action.channel.serverId
-    //   );
-    //   currentServer.channels.push(newChannel);
-    //   // newState[Object.values(newState)[0]].servers[].channels.push(action.channel);
-    //   return newState;
+    case RECEIVE_SERVER:
+      // debugger;
+      const newServer = {
+        id: action.server.id,
+        serverName: action.server.serverName,
+        channels: action.server.channels,
+      };
+      const servers = Object.values(newState)[0].servers;
+
+      for (let server of servers) {
+        if (server.id === newServer.id) {
+          return oldState;
+        }
+      }
+
+      servers.push(newServer);
+      return newState;
+    case RECEIVE_CHANNEL:
+      // debugger;
+      const newChannel = {
+        id: action.channel.id,
+        channelName: action.channel.channelName,
+        serverId: action.channel.serverId,
+      };
+
+      const currentServer = Object.values(newState)[0].servers.find(
+        (server) => server.id === action.channel.serverId
+      );
+
+      const channels = currentServer.channels;
+
+      for(let channel of channels){
+        if(channel.id === newChannel.id){
+          return oldState;
+        }
+      }
+
+      channels.push(newChannel);
+      return newState;
     default:
       return oldState;
   }
