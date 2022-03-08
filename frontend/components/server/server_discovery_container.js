a72a7b410c8a92d73baafe48f576b0a42c3c55ff
egumin <ethangumin@gmail.com>
2022-03-08 16:55:14 -0500

    cross browser css

---
+++ frontend/components/server/server_discovery_container.js
@@ -0,0 +1,12 @@
+import { connect } from "react-redux";
+import ServerDiscovery from "./server_discovery";
+
+const mSTP = (state) => {
+  return {};
+};
+
+const mDTP = (dispatch) => {
+  return {};
+};
+
+export default connect(mSTP, mDTP)(ServerDiscovery);
