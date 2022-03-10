f49f21b0b8097022b910ebcc3fb591f9b87504da
egumin <ethangumin@gmail.com>
2022-03-10 01:20:52 -0500

    protected routes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import { AuthRoute } from "../util/route_util";
+import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import SplashContainer from "./splash/splash_container";
 import SignUpFormContainer from "./auth/signup_form_container";
 import LoginFormContainer from "./auth/login_form_container";
@@ -15,13 +15,16 @@ const App = () => {
         <Route exact path="/" component={SplashContainer} />
         <AuthRoute path="/signup" component={SignUpFormContainer} />
         <AuthRoute path="/login" component={LoginFormContainer} />
-        <Route path="/home" component={HomeIndexContainer} />
-        <Route
+        <ProtectedRoute path="/home" component={HomeIndexContainer} />
+        <ProtectedRoute
           exact
           path="/server/:id/channel/:channelId"
           component={ServerContainer}
         />
-        <Route path="/server-discovery" component={ServerDiscoveryContainer} />
+        <ProtectedRoute
+          path="/server-discovery"
+          component={ServerDiscoveryContainer}
+        />
       </Switch>
     </div>
   );
