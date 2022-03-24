688c2bc544e67b74f977125bbba6a2d247420e5c
egumin <ethangumin@gmail.com>
2022-03-24 17:35:10 -0400

    convert ServerChannels to functional component

--- frontend/components/server/server_channels.jsx
+++ frontend/components/server/server_channels.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, withRouter } from "react-router-dom";
 import Hashtag from "../../../app/assets/images/hashtag-solid.svg";
 import DeleteBtn from "../../../app/assets/images/xmark-solid.svg";
@@ -6,138 +6,128 @@ import EditBtn from "../../../app/assets/images/pen-to-square-solid.svg";
 import ChannelModal from "../modals/channel_modal";
 import EditChannelModal from "../modals/edit_channel_modal";
 
-class ServerChannels extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { channelModalActive: false, editChannelModalActive: false };
-    this.toggleChannelModal = this.toggleChannelModal.bind(this);
-    this.toggleEditChannelModal = this.toggleEditChannelModal.bind(this);
-  }
+const ServerChannels = (props) => {
+  const [channelModalActive, setChannelModalActive] = useState(false);
+  const [editChannelModalActive, setEditChannelModalActive] = useState(false);
 
-  toggleChannelModal() {
-    this.setState({ channelModalActive: !this.state.channelModalActive });
-  }
+  const toggleChannelModal = () => {
+    setChannelModalActive(!channelModalActive);
+  };
 
-  toggleEditChannelModal() {
-    this.setState({
-      editChannelModalActive: !this.state.editChannelModalActive,
-    });
-  }
+  const toggleEditChannelModal = () => {
+    setEditChannelModalActive(!editChannelModalActive);
+  };
 
-  render() {
-    const mapChannelsToServer =
-      this.props.server && this.props.currentChannels
-        ? this.props.currentChannels
-            .filter((channel) => channel.serverId === this.props.server.id)
-            .map((channel, index) => {
-              return (
-                <div
-                  key={channel.id}
-                  className={
-                    this.props.currentChannelId === channel.id
-                      ? "server-channels__active server-channels__channel"
-                      : "server-channels__inactive server-channels__channel"
-                  }
+  const mapChannelsToServer =
+    props.server && props.currentChannels
+      ? props.currentChannels
+          .filter((channel) => channel.serverId === props.server.id)
+          .map((channel, index) => {
+            return (
+              <div
+                key={channel.id}
+                className={
+                  props.currentChannelId === channel.id
+                    ? "server-channels__active server-channels__channel"
+                    : "server-channels__inactive server-channels__channel"
+                }
+              >
+                <Link
+                  to={`/server/${props.server.id}/channel/${channel.id}`}
+                  className="server-channels__route"
                 >
-                  <Link
-                    to={`/server/${this.props.server.id}/channel/${channel.id}`}
-                    className="server-channels__route"
-                  >
-                    <div className="server-channels__name-hash">
-                      <img
-                        src={Hashtag}
-                        alt="hashtag"
-                        className="channel-pound"
-                      />
-                      <p>{channel.channelName}</p>
-                    </div>
-                  </Link>
-                  <div>
+                  <div className="server-channels__name-hash">
                     <img
-                      src={DeleteBtn}
-                      alt="delete channel button"
-                      onClick={() =>
-                        this.props
-                          .deleteChannel(channel.id)
-                          .then(() =>
-                            this.props.history.push(
-                              `/server/${this.props.server.id}/channel/${
-                                this.props.currentChannels.find(
-                                  (channel) =>
-                                    channel.server_id === this.props.serverId
-                                ).id
-                              }`
-                            )
-                          )
-                      }
-                      className={
-                        index === 0 ||
-                        this.props.currentUser.id !==
-                          this.props.server.creatorId
-                          ? "hide-channel"
-                          : "delete-channel-btn"
-                      }
+                      src={Hashtag}
+                      alt="hashtag"
+                      className="channel-pound"
                     />
-                    <img
-                      src={EditBtn}
-                      alt="edit channel"
-                      className={
-                        this.props.currentUser.id !==
-                        this.props.server.creatorId
-                          ? "hide-channel"
-                          : "edit-channel-btn"
-                      }
-                      onClick={() => this.toggleEditChannelModal()}
-                    />
+                    <p>{channel.channelName}</p>
                   </div>
-                  <EditChannelModal
-                    currentChannel={channel}
-                    updateChannel={this.props.updateChannel}
-                    currentUserId={this.props.currentUser.id}
-                    serverCreatorId={this.props.server.creatorId}
-                    toggleEditChannelModal={this.toggleEditChannelModal}
-                    active={this.state.editChannelModalActive}
+                </Link>
+                <div>
+                  <img
+                    src={DeleteBtn}
+                    alt="delete channel button"
+                    onClick={() =>
+                      props
+                        .deleteChannel(channel.id)
+                        .then(() =>
+                          props.history.push(
+                            `/server/${props.server.id}/channel/${
+                              props.currentChannels.find(
+                                (channel) =>
+                                  channel.server_id === props.serverId
+                              ).id
+                            }`
+                          )
+                        )
+                    }
+                    className={
+                      index === 0 ||
+                      props.currentUser.id !== props.server.creatorId
+                        ? "hide-channel"
+                        : "delete-channel-btn"
+                    }
+                  />
+                  <img
+                    src={EditBtn}
+                    alt="edit channel"
+                    className={
+                      props.currentUser.id !== props.server.creatorId
+                        ? "hide-channel"
+                        : "edit-channel-btn"
+                    }
+                    onClick={() => toggleEditChannelModal()}
                   />
                 </div>
-              );
-            })
-        : "";
+                <EditChannelModal
+                  currentChannel={channel}
+                  updateChannel={props.updateChannel}
+                  currentUserId={props.currentUser.id}
+                  serverCreatorId={props.server.creatorId}
+                  toggleEditChannelModal={toggleEditChannelModal}
+                  active={editChannelModalActive}
+                />
+              </div>
+            );
+          })
+      : "";
 
-    const createChannelButton =
-      this.props.server &&
-      this.props.currentUser &&
-      this.props.server.creatorId === this.props.currentUser.id ? (
-        <p onClick={(e) => this.toggleChannelModal(e)}>+</p>
-      ) : (
-        <p></p>
-      );
-
-    return (
-      <div className="server-channels__container">
-        <h3 className="server-channels__header">
-          {this.props.server ? this.props.server.serverName : ""}
-        </h3>
-        <div className="server-channels__channels-idx">
-          <div>
-            <div className="server-channels__text-channels-header">
-              <p>TEXT CHANNELS</p>
-              {createChannelButton}
-            </div>
-            <ul>{mapChannelsToServer}</ul>
-          </div>
-          <div className="server-channels__curr-user">
-            <p></p>
-            <p>{this.props.currentUser.username}</p>
-          </div>
-        </div>
-        <ChannelModal
-          channelModalActive={this.state.channelModalActive}
-          toggleChannelModal={this.toggleChannelModal}
-          createChannel={this.props.createChannel}
-        />
-      </div>
+  const createChannelButton =
+    props.server &&
+    props.currentUser &&
+    props.server.creatorId === props.currentUser.id ? (
+      <p onClick={(e) => toggleChannelModal(e)}>+</p>
+    ) : (
+      <p></p>
     );
-  }
-}
+
+  return (
+    <div className="server-channels__container">
+      <h3 className="server-channels__header">
+        {props.server ? props.server.serverName : ""}
+      </h3>
+      <div className="server-channels__channels-idx">
+        <div>
+          <div className="server-channels__text-channels-header">
+            <p>TEXT CHANNELS</p>
+            {createChannelButton}
+          </div>
+          <ul>{mapChannelsToServer}</ul>
+        </div>
+        <div className="server-channels__curr-user">
+          <p></p>
+          <p>{props.currentUser.username}</p>
+        </div>
+      </div>
+      <ChannelModal
+        channelModalActive={channelModalActive}
+        toggleChannelModal={toggleChannelModal}
+        createChannel={props.createChannel}
+      />
+    </div>
+  );
+};
 
 export default withRouter(ServerChannels);
