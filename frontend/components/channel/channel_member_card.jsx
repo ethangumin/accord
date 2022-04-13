41f22a287907ff426afd4102765fb301a3732f68
egumin <ethangumin@gmail.com>
2022-04-13 18:15:21 -0400

    ChannelMemberCard styling

--- frontend/components/channel/channel_member_card.jsx
+++ frontend/components/channel/channel_member_card.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-const ChannelMemberCard = () => {
+const ChannelMemberCard = (props) => {
+  const dispatch = useDispatch();
+
+  const friends = useSelector((state) => state.entities.friends);
+  const currentUser = useSelector(
+    (state) => state.entities.users[state.session.id]
+  );
+
+  const cardAction =
+    props.currentUser?.id in friends ? (
+      <input
+        type="text"
+        placeholder={`Message @${props.currentUser.username}`}
+      />
+    ) : currentUser.id === props.currentUser?.id ? (
+      ""
+    ) : (
+      <button>Add Friend</button>
+    );
+
   return (
-    <div>ChannelMemberCard</div>
-  )
-}
+    <div
+      className={
+        props.cardActive
+          ? "channel-member-card__container"
+          : "hidden"
+      }
+      style={props.style}
+    >
+      <div className="channel-member-card__icon">
+        {props.currentUser?.username[0]}
+      </div>
+      <p className="channel-member-card__username">{props.currentUser?.username}</p>
+      {cardAction}
+    </div>
+  );
+};
 
-export default ChannelMemberCard
+export default ChannelMemberCard;
--- frontend/components/channel/channel_members.jsx
+++ frontend/components/channel/channel_members.jsx
@@ -1,13 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchServerMembers } from "../../actions/server_member_actions";
+import ChannelMemberCard from "./channel_member_card";
 
 const ChannelMembers = (props) => {
   const dispatch = useDispatch();
-
+  const [cardActive, setCardActive] = useState(false);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [pos, setPos] = useState({ x: "0px", y: "0px" });
   const serverMembers = useSelector((state) =>
     Object.values(state.entities.serverMembers)
   );
+  // const loggedInUser = useSelector(
+  //   (state) => state.entities.users[state.session.id]
+  // );
 
   useEffect(() => {
     if (props.currentChannel) {
@@ -15,8 +21,34 @@ const ChannelMembers = (props) => {
     }
   }, [props.currentChannel]);
 
+  useEffect(() => {
+    const controller = new AbortController();
+
+    window.addEventListener("click", (e) => {
+      e.preventDefault();
+      setCardActive(false);
+    });
+
+    return () => controller.abort;
+  }, []);
+
+  const toggleCardHandler = (e, user) => {
+    e.stopPropagation();
+
+    const xPos = e.pageY - 60 + "px";
+    const yPos = e.pageX - 200 + "px";
+
+    setPos({ x: xPos, y: yPos });
+    setCardActive(true);
+    setCurrentUser(user);
+  };
+
   const serverMembersDisplay = serverMembers.map((user) => (
-    <div className="channel-content__member" key={user.id}>
+    <div
+      className="channel-content__member"
+      key={user.id}
+      onClick={(e) => toggleCardHandler(e, user)}
+    >
       <p className="channel-content__member-icon">{user.username[0]}</p>
       <p>
         {user.username.length < 12
@@ -30,6 +62,12 @@ const ChannelMembers = (props) => {
     <div className="channel-content__members">
       <h3>SERVER MEMBERS - {serverMembers.length}</h3>
       {serverMembersDisplay}
+      <ChannelMemberCard
+        cardActive={cardActive}
+        currentUser={currentUser}
+        pos={pos}
+        style={{ top: pos.x, left: pos.y}}
+      />
     </div>
   );
 };
