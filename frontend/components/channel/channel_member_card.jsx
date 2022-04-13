0342a52836c073f489c3728fffdee955a2035353
egumin <ethangumin@gmail.com>
2022-04-13 18:46:10 -0400

    users able to add friends and create/navigate to messages from ServerMembers

--- frontend/components/channel/channel_member_card.jsx
+++ frontend/components/channel/channel_member_card.jsx
@@ -1,42 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
+import { withRouter } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
+import { createFriendship } from "../../actions/friendship_actions";
+import { createConversation } from "../../actions/conversation_actions";
 
 const ChannelMemberCard = (props) => {
   const dispatch = useDispatch();
 
+  // const [message, setMessage] = useState("");
   const friends = useSelector((state) => state.entities.friends);
   const currentUser = useSelector(
     (state) => state.entities.users[state.session.id]
   );
+  const conversation = useSelector((state) => {
+    for (let conversation of Object.values(state.entities.conversations)) {
+      if (
+        conversation.user2Id === props.currentUser?.id ||
+        conversation.user1Id === props.currentUser?.id
+      ) {
+        return conversation;
+      }
+    }
+    return null;
+  });
+
+  const addFriendHandler = (e, friendId) => {
+    e.preventDefault();
+    e.stopPropagation();
+    dispatch(createFriendship(friendId));
+  };
+
+  const startConversationHandler = (e, user) => {
+    e.preventDefault();
+    if (conversation) {
+      debugger;
+      props.history.push(`/conversation/${conversation.id}`);
+    } else {
+      debugger;
+      dispatch(createConversation(user.id)).then((conversation) => {
+        debugger;
+        props.history.push(`/conversation/${conversation.conversation.id}`);
+      });
+    }
+  };
+
+  // const cardAction =
+  //   props.currentUser?.id in friends ? (
+  //     <input
+  //       type="text"
+  //       placeholder={`Message @${props.currentUser.username}`}
+  //       onChange={e => setMessage(e)}
+  //       onClick={e => e.stopPropagation()}
+  //     />
+  //   ) : currentUser.id === props.currentUser?.id ? (
+  //     ""
+  //   ) : (
+  //     <button onClick={(e) => addFriendHandler(e, props.currentUser?.id)}>Add Friend</button>
+  //   );
 
   const cardAction =
     props.currentUser?.id in friends ? (
-      <input
-        type="text"
-        placeholder={`Message @${props.currentUser.username}`}
-      />
+      <button onClick={(e) => startConversationHandler(e, props.currentUser)}>
+        Start Conversation
+      </button>
     ) : currentUser.id === props.currentUser?.id ? (
       ""
     ) : (
-      <button>Add Friend</button>
+      <button onClick={(e) => addFriendHandler(e, props.currentUser?.id)}>
+        Add Friend
+      </button>
     );
 
   return (
     <div
-      className={
-        props.cardActive
-          ? "channel-member-card__container"
-          : "hidden"
-      }
+      className={props.cardActive ? "channel-member-card__container" : "hidden"}
       style={props.style}
     >
       <div className="channel-member-card__icon">
         {props.currentUser?.username[0]}
       </div>
-      <p className="channel-member-card__username">{props.currentUser?.username}</p>
+      <p className="channel-member-card__username">
+        {props.currentUser?.username}
+      </p>
       {cardAction}
     </div>
   );
 };
 
-export default ChannelMemberCard;
+export default withRouter(ChannelMemberCard);
